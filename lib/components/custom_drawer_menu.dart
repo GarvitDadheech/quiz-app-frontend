83d95efea60fdb5c3220db80ce85f1e677a5042f
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-08-18 11:54:20 +0530

    implement delete user functionality

--- lib/components/custom_drawer_menu.dart
+++ lib/components/custom_drawer_menu.dart
@@ -1,13 +1,55 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart'; // Import url_launcher
-import '../pages/login.dart'; // Import your LoginPage
+import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import '../pages/login.dart';
 
 class CustomDrawer extends StatelessWidget {
+  Future<void> _deleteUserAccount(BuildContext context) async {
+    final prefs = await SharedPreferences.getInstance();
+    final userId = prefs.getInt('userId');
+    if (userId == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('User ID not found.')),
+      );
+      return;
+    }
+
+    final url = 'http://localhost:8080/delete-user/$userId';
+    final response = await http.delete(Uri.parse(url));
+  
+    if (response.statusCode == 200) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Account deleted successfully.')),
+      );
+      await prefs.remove('userId');
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+        (route) => false,
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to delete account.')),
+      );
+    }
+  }
+
+  Future<void> _navigateToLoginPage(BuildContext context) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('userId');
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => LoginPage()),
+      (route) => false,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: Container(
-        color: Color(0xFF9370DB), // Light purple background
+        color: Color(0xFF9370DB),
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
@@ -21,7 +63,8 @@ class CustomDrawer extends StatelessWidget {
                   SizedBox(height: 10),
                   ElevatedButton(
                     onPressed: () async {
-                      const url = 'https://www.instagram.com/threeinvesteers/?hl=en';
+                      const url =
+                          'https://www.instagram.com/threeinvesteers/?hl=en';
                       if (await canLaunch(url)) {
                         await launch(url);
                       } else {
@@ -29,7 +72,7 @@ class CustomDrawer extends StatelessWidget {
                       }
                     },
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xFF00C18E), // Follow us button color
+                      backgroundColor: Color(0xFF00C18E),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(20),
                       ),
@@ -41,7 +84,8 @@ class CustomDrawer extends StatelessWidget {
             ),
             ListTile(
               leading: Icon(Icons.share, color: Colors.white),
-              title: Text('Share this app', style: TextStyle(color: Colors.white)),
+              title:
+                  Text('Share this app', style: TextStyle(color: Colors.white)),
               onTap: () {},
             ),
             ListTile(
@@ -56,22 +100,23 @@ class CustomDrawer extends StatelessWidget {
             ),
             ListTile(
               leading: Icon(Icons.refresh, color: Colors.white),
-              title: Text('Reset account', style: TextStyle(color: Colors.white)),
+              title:
+                  Text('Reset account', style: TextStyle(color: Colors.white)),
               onTap: () {},
             ),
             ListTile(
               leading: Icon(Icons.delete, color: Colors.white),
-              title: Text('Delete account', style: TextStyle(color: Colors.white)),
-              onTap: () {},
+              title:
+                  Text('Delete account', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                _deleteUserAccount(context);
+              },
             ),
             ListTile(
               leading: Icon(Icons.login, color: Colors.white),
               title: Text('Login', style: TextStyle(color: Colors.white)),
               onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-                );
+                _navigateToLoginPage(context);
               },
             ),
             Padding(
@@ -79,7 +124,8 @@ class CustomDrawer extends StatelessWidget {
               child: Divider(color: Colors.white),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10),
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
